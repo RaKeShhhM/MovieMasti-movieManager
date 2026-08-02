f0ef4e4399b0ec34e1bd8c9b60585cba83c2e4be
rakesh mahato <rm115275@gmai.com>
2026-08-02 22:46:38 +0530

    revert deployment serving and API URL configurations

--- backend/index.js
+++ backend/index.js
@@ -33,17 +33,6 @@ app.use("/api/v1/movies", moviesRoutes);
 app.use("/api/v1/upload", uploadRoutes);
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
